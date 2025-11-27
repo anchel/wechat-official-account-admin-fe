757e8cf6d3f1bb4232d0afb4b9738e93906329ae
anchel <anchel.xy@163.com>
2025-11-27 14:06:51 +0800

    feat: 引入 ageis

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+import Aegis from 'aegis-web-sdk'
+const aegis = new Aegis({
+  id: '4eog2C5DvxZjRW5pvn', // 上报 id
+  uin: '', // 用户唯一 ID（可选）
+  reportApiSpeed: true, // 接口测速
+  reportAssetSpeed: true, // 静态资源测速
+  spa: true, // spa 应用页面跳转的时候开启 pv 计算
+  hostUrl: 'https://rumt-zh.com',
+})
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
